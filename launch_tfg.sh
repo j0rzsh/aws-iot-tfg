da1231fff39662e8377c2bafdff663a7b8c60e47
j0rzsh <jorge2006nieva@gmail.com>
2020-05-16 19:30:10 +0200

    launching script modifications

--- launch_tfg.sh
+++ launch_tfg.sh
@@ -1,24 +1,30 @@
 #! /bin/bash
 
+echo "####################";
+echo "Starting launch_tfg";
+echo "####################";
 ## Create infrastructure
+echo "Stage 1/3: Create infrastructure";
 echo "Launching terraform . . .";
-echo "terraform workspace new dev";
+echo "Step 1/3: terraform workspace new dev";
 terraform workspace new dev;
-echo "terraform workspace select dev";
+echo "Step 2/3: terraform workspace select dev";
 terraform workspace select dev;
-echo "terraform apply -var-file aws-iot-tfg-dev.tfvars -auto-approve";
+echo "Step 3/3: terraform apply -var-file aws-iot-tfg-dev.tfvars -auto-approve";
 terraform apply -var-file aws-iot-tfg-dev.tfvars -auto-approve;
 
 ## Save terraform outputs
+echo "Stage 2/3: Save terraform outputs";
 echo "Saving terraform outputs . . .";
-echo "cd scripts";
+echo "Step 1/3: cd scripts";
 cd scripts;
-echo "python3 save_outputs_terraform.py";
+echo "Step 2/3: python3 save_outputs_terraform.py";
 python3 save_outputs_terraform.py;
-echo "cd ..";
+echo "Step 3/3: cd ..";
 cd ..;
 
 ## Copy data to simulator EC2
+echo "Stage 3/3: Copy data to simulator EC2";
 echo "Save simulator's IP . . .";
 IP_SIMULATOR=$(cat config/simulator_ip)
 echo "Copy simulator files to EC2 . . ."
